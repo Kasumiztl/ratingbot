8383f2cec7052d75fff6e364a547a9193d93bd56
Kasumiztl <sivakanastasia558@gmail.com>
2025-08-31 19:17:10 +0300

    Add files via upload

--- rating.py
+++ rating.py
@@ -205,7 +205,7 @@ async def edit_user(message: types.Message):
 
     args = message.text.split(maxsplit=3)
     if len(args) < 4:
-        await message.answer("Использование: /edituser @username новая роль @newusername")
+        await message.answer("Использование: /edit_user @username новая роль @newusername")
         return
 
     old_username, new_role, new_username = args[1], args[2], args[3]
@@ -224,7 +224,7 @@ async def remove_user(message: types.Message):
 
     args = message.text.split()
     if len(args) < 2:
-        await message.answer("Использование: /removeuser @username")
+        await message.answer("Использование: /remove_user @username")
         return
 
     username = args[1]
